227329b4f7c1d2db017365bffe974ee1b3380485
demi0504 <ripley.demi@gmail.com>
2020-09-30 00:48:37 -0500

    got a navbar working. whoa

---
+++ portfolio/src/components/Navbar/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+function Navbar() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <Link className="navbar-brand" to="/">
+          Demi Ripley
+        </Link>
+        <div>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link
+                to="/"
+                className={
+                  window.location.pathname === "/" || window.location.pathname === "/home"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/contact"
+                className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+              >
+                Contact
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/portfolio"
+                className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
+              >
+                Portfolio
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+  
+  export default Navbar;
---
+++ portfolio/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import Jumbotron from "../components/Jumbotron";
+
+function Home() {
+  return (
+    <div>
+      
+        <h1>Demi</h1>
+        
+    </div>
+  )
+}
+
+export default Home;
