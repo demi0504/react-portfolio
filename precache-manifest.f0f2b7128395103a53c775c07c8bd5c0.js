2043df5db137d0e53f1147dc0b438b6c292b079b
demi0504 <ripley.demi@gmail.com>
2020-10-03 16:11:17 -0500

    Updates

---
+++ precache-manifest.f0f2b7128395103a53c775c07c8bd5c0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f6d35e6ac500cb7230742a8226ef06",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "98c2f0d5aa5b979d8894",
+    "url": "/react-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9d28ac1051487b6405fe",
+    "url": "/react-portfolio/static/css/main.e770fffe.chunk.css"
+  },
+  {
+    "revision": "98c2f0d5aa5b979d8894",
+    "url": "/react-portfolio/static/js/2.2ddc6537.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.2ddc6537.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d28ac1051487b6405fe",
+    "url": "/react-portfolio/static/js/main.b160d026.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "833367f1b970caa32830f83de0dff523",
+    "url": "/react-portfolio/static/media/Screenshot (16).833367f1.png"
+  },
+  {
+    "revision": "b844e4a3b958a4e3e018d15f69ad254a",
+    "url": "/react-portfolio/static/media/Screenshot (18).b844e4a3.png"
+  },
+  {
+    "revision": "aa823098511395af7bf08d748bfd9ec4",
+    "url": "/react-portfolio/static/media/Screenshot (19).aa823098.png"
+  },
+  {
+    "revision": "1551b5653a904e0f82cb478223e57559",
+    "url": "/react-portfolio/static/media/Screenshot (26).1551b565.png"
+  },
+  {
+    "revision": "44b718ceea64cb3899c69e8a1741dcd6",
+    "url": "/react-portfolio/static/media/Screenshot (28).44b718ce.png"
+  },
+  {
+    "revision": "b05faaf1cb5fc3cbdc47fdad89642096",
+    "url": "/react-portfolio/static/media/Screenshot (31).b05faaf1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.a0ded5d33985c855fa1251d57b43910e.js"
+  "/react-portfolio/precache-manifest.f0f2b7128395103a53c775c07c8bd5c0.js"
 );
 
 self.addEventListener('message', (event) => {
