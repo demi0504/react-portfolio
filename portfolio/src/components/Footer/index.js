a7a92d2c37d233272fc2c58afc24a225723c8cc1
demi0504 <ripley.demi@gmail.com>
2020-10-03 16:48:53 -0500

    added footer social tags and styling

--- portfolio/src/components/Footer/index.js
+++ portfolio/src/components/Footer/index.js
@@ -3,9 +3,15 @@ import "./style.css";
 
 function Footer() {
   return (
-    <footer className="footer py-4 bg-dark text-white-50">
-      <span>Demi</span>
-    </footer>
+    <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
+        <div className="container text-center">
+          <a href="https://github.com/demi0504" target="_blank" rel="noopener noreferrer" className="fa fa-github"></a>
+          <a href="https://www.linkedin.com/in/demi-ripley-224759118/" target="_blank" rel="noopener noreferrer" className="fa fa-linkedin"></a>
+          <a href="https://www.facebook.com/demi.ann.14" target="_blank" rel="noopener noreferrer" className="fa fa-facebook"></a>
+          <a href="https://twitter.com/demiasinhalf" target="_blank" rel="noopener noreferrer" className="fa fa-twitter"></a>
+          <a href="https://www.instagram.com/demiasinhalf/?hl=en" target="_blank" rel="noopener noreferrer" className="fa fa-instagram"></a>
+        </div>
+      </footer>
   );
 }
 
